936fec19650f187e2701c576334e49e3247eb926
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-01-29 13:17:03 +0545

    ```printCommandLineArgumentFromAIndex``` func

--- helloworld.go
+++ helloworld.go
@@ -7,15 +7,25 @@ import (
 
 func main() {
 	fmt.Println(printSpaceSeparatedCommandLineArgument(os.Args))
+	fmt.Println(printCommandLineArgumentFromAIndex(os.Args, 3))
 }
 
 func printSpaceSeparatedCommandLineArgument (args []string) string {
 	var res, sep string
-	for i := 0; i < len(os.Args); i++ {
+	for i := 1; i < len(os.Args); i++ {
 		res += sep + os.Args[i]
 		sep = " "
 	}
 
+	return res
+}
+
+func printCommandLineArgumentFromAIndex (args []string, index int) string {
+	var res, sep string
+	for _, args := range os.Args[index:] {
+		res += sep + args
+		sep = " "
+	}
 	return res
 }
 
